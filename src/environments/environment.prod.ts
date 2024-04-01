42d1164a073c647680e7af7525a01647da7f0d84
13Andrei13 <andrei.buric@yahoo.com>
2024-04-01 21:29:32 +0300

    project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAH3HqudK4n3wFiXiQ4XHHfjwo9tDklLJg',
+    authDomain: 'clips-27626.firebaseapp.com',
+    projectId: 'clips-27626',
+    storageBucket: 'clips-27626.appspot.com',
+    appId: '1:235044264657:web:14dee996602f6694b2c3f0',
+  },
+};
